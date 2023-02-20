4a72ec2f9e4ea408eb5a7298ff15f1800bc4b85e
Davorin Haberman <125305007+dhbrmn@users.noreply.github.com>
2023-02-20 19:54:47 +0100

    completed tasks 13/14

--- caesar_cipher/caesar_cipher.cs
+++ caesar_cipher/caesar_cipher.cs
@@ -1,1 +1,33 @@
+using System;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices;
 
+namespace ExquisiteCorpse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+            Console.WriteLine("Input the secret message: ");
+            string msgString = Console.ReadLine();
+
+            char[] msgArray = msgString.ToCharArray();
+
+            char[] ecnryptedMsg = new char[msgString.Length];
+
+            for (int i = 0; i < msgString.Length; i++)
+            {
+                char a = msgArray[i];
+                int alphaInx = Array.IndexOf(alphabet, a);
+                alphaInx = (alphaInx + 3) % 26;
+                char encChar = alphabet[alphaInx];
+                ecnryptedMsg[i] = encChar;
+            }
+
+            string encString = String.Join("", ecnryptedMsg);
+            Console.WriteLine(encString);
+        }
+    }
+}
