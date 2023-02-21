e7afcfb05e53dc46904c8aecbabf454917580008
Davorin Haberman <125305007+dhbrmn@users.noreply.github.com>
2023-02-21 20:26:12 +0100

    answers to questions being stored correctly

--- true_false/true_false.cs
+++ true_false/true_false.cs
@@ -1,1 +1,50 @@
+using System;
+
+namespace TrueOrFalse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Do not edit these lines
+            Console.WriteLine("Welcome to 'True or False?'\nPress Enter to begin:");
+            string entry = Console.ReadLine();
+            Tools.SetUpInputStream(entry);
+
+            // arrays
+            string[] questions = { "Are roses red?", "Is iron a liquid?", "Do pigs fly?", "Is the world round?", "Does anything taste better than pizza?" };
+            bool[] answers = { true, false, false, true, false };
+            bool[] responses = new bool[questions.Length];
+
+            //array equal length check
+            if (questions.Length != answers.Length)
+            {
+                Console.WriteLine("Error 101: Strings unequal length!");
+            }
+
+            //foreach question loop
+            int askingIndex = 0;
+            foreach (string question in questions)
+            {
+                /*print question take answer*/
+                Console.WriteLine($"{questions[askingIndex]} True or false?");
+                string input = Console.ReadLine().ToLower();
+               
+                /*convert answer to bool*/
+                bool isBool = bool.TryParse(input, out bool inputBool);
+
+                while (isBool == false)
+                {
+                    Console.WriteLine("Unsupported answer. Please only input \"true\" or \"false\"");
+                    input = Console.ReadLine().ToLower();
+                    isBool = Boolean.TryParse(input, out inputBool);
+                }
+                
+                    responses[askingIndex] = inputBool;
+                        ++askingIndex;
+            }
+
+        }
+    }
+}
 
