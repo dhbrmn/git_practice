3a79656193c73afdf3a658f06ff0d1e75d412515
dhbrmn <haberman.davorin@gmail.com>
2023-02-22 07:50:04 +0100

    finished rework with methods

--- true_false/true_false.cs
+++ true_false/true_false.cs
@@ -6,36 +6,43 @@ namespace TrueOrFalse
     {
         static void Main(string[] args)
         {
-            //Make whole app run with a single method RunQuiz(questions[], answers[])
             // Do not edit these lines
             Console.WriteLine("Welcome to 'True or False?'\nPress Enter to begin:");
             string entry = Console.ReadLine();
             Tools.SetUpInputStream(entry);
-
-            RunQuiz();
-          /*  
-            // arrays
+           
+            // INPUT ARRAYS
             string[] questions = { "Are roses red?", "Is iron a liquid?", "Do pigs fly?", "Is the world round?", "Does anything taste better than pizza?" };
             bool[] answers = { true, false, false, true, false };
-            bool[] responses = new bool[questions.Length];
+
+            RunQuiz(questions, answers);
+            RunQuiz();
+
+        }
+
+        static void RunQuiz(string[] questions, bool[] answers)
+        {
+            //METHOD TO RUN QUIZ WITH PREDEFINED QUESTIONS AND ANSWERS
+
 
             //array equal length check
             if (questions.Length != answers.Length)
             {
-                Console.WriteLine("Error 101: Strings unequal length!");
+                Console.WriteLine("Error 101: Strings unequal length! Press Enter to restart.");
+                Console.ReadLine();
             }
 
             //foreach loop
             int askingIndex = 0;
             int score = 0;
-
+            bool[] responses = new bool[questions.Length];
 
             foreach (string question in questions)
             {
                 //print question take answer
                 Console.WriteLine($"{questions[askingIndex]} True or false?");
                 string input = Console.ReadLine().ToLower();
-               
+
                 //convert answer to bool
                 bool isBool = bool.TryParse(input, out bool inputBool);
 
@@ -45,9 +52,9 @@ namespace TrueOrFalse
                     input = Console.ReadLine().ToLower();
                     isBool = Boolean.TryParse(input, out inputBool);
                 }
-                
-                    responses[askingIndex] = inputBool;
-                        
+
+                responses[askingIndex] = inputBool;
+
 
                 bool ans = answers[askingIndex];
                 bool res = responses[askingIndex];
@@ -64,12 +71,7 @@ namespace TrueOrFalse
 
             }
 
-            Console.WriteLine($"You managed to get {score} out of 5 correct!");*/
-        }
-
-        static void RunQuiz(string[] questions, bool[] answers)
-        {
-
+            Console.WriteLine($"You managed to get {score} out of 5 correct!"); 
         }
 
 
