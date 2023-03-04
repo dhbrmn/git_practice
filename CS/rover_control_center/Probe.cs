c7102389278a8a0c474dbc7edef0c9c55bf6c4e1
Davorin Haberman <125305007+dhbrmn@users.noreply.github.com>
2023-03-04 09:51:40 +0100

    Task 10/10 Probe.cs

--- CS/rover_control_center/Probe.cs
+++ CS/rover_control_center/Probe.cs
@@ -2,6 +2,19 @@ namespace RoverControlCenter
 {
     class Probe
     {
-
-    }
+        namespace RoverControlCenter
+        {
+        abstract class Probe
+         {
+              public string Alias
+                {
+                 get; protected set;
+                }
+        
+             public abstract string GetInfo();
+             public abstract string Explore();
+             public abstract string Collect();       
+        }
+       }
+   }
 }
