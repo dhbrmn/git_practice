139a1a35c94db94c4b1b00d1a35281c4e5d1725c
Davorin Haberman <125305007+dhbrmn@users.noreply.github.com>
2023-10-06 17:37:54 +0200

    Donno what I am doing but it's okaaaay

---
+++ cs/note_taking_app/NoteTakingApp/NotemanLibrary/Models/AttachmentModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotemanLibrary.Models
+{
+    public class AttachmentModel
+    {
+        /// <summary>
+        /// Unique identifier for every attachment stored in database
+        /// </summary>
+        public int ID
+        {
+        get; set; 
+        }
+        /// <summary>
+        /// Name of the file
+        /// </summary>
+        public string Name
+        {
+        get; set; 
+        }
+        /// <summary>
+        /// Path to file stored as string
+        /// </summary>
+        public string AttachmentPath
+        {
+        get; set; 
+        }
+        /// <summary>
+        /// Fereign key identifier to link a note to an attachment
+        /// </summary>
+        public int NoteId
+        {
+        get; set; 
+        }
+        /// <summary>
+        /// Date of storing attachment model in database
+        /// </summary>
+        public string Date
+        {
+            get; set;
+        }
+        /// <summary>
+        /// Generic class setting method
+        /// </summary>
+        public AttachmentModel()
+        {
+        }
+        /// <summary>
+        /// Method to set an attachment model without note.ID
+        /// </summary>
+        /// <param name="path">Attachment path</param>
+        public AttachmentModel( string path )
+        {
+            AttachmentPath = path;
+            string name = Path.GetFileName(path);
+            Name = name;
+            string date = DateTime.Now.ToString();
+            Date = date;
+
+        }
+        /// <summary>
+        /// Method to set a model linking the file path and NoteModel
+        /// </summary>
+        /// <param name="path">File path</param>
+        /// <param name="note">NoteModel of the created note</param>
+        public AttachmentModel( string path, NoteModel note )
+        {
+            AttachmentPath = path;
+            string name = Path.GetFileName(path);
+            Name = name;
+            NoteId = note.ID;
+            string date = DateTime.Now.ToString();
+            Date = date;
+        }
+    }
+}
